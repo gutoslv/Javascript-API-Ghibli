49e52ee0d7a2266c9921b4606309b1c4b232a11e
Augusto Cesar <guto_slv@live.com>
2018-09-10 09:04:50 -0300

    Adds error treatment for failed request

--- script.js
+++ script.js
@@ -2,17 +2,20 @@
 let request = new XMLHttpRequest();
 
 //Open a new connectoin, using the GET request on the URL endpoint
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open("GET", "https://ghibliapi.herokuapp.com/films", true);
 
-request.onload = function () {
-    //Begin accessing JSON data here
-    let data = JSON.parse(this.response);
+request.onload = function() {
+  //Begin accessing JSON data here
+  let data = JSON.parse(this.response);
 
+  if (request.status >= 200 && request.status < 400) {
     data.forEach(movie => {
-        // Log each movie's title
-        console.log(movie.title);
-    })
-}
-
+      // Log each movie's title
+      console.log(movie.title);
+    });
+  } else {
+    console.log("error");
+  }
+};
 //Send request
 request.send();
